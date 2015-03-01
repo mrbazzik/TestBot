2dcdaa9d6ccec281bf7c4c94bba898de009887a0
Igor Basov <mrbazzik@gmail.com>
2015-03-01 18:21:26 +0300

    OK

---
+++ background.js
@@ -0,0 +1,9 @@
+﻿$.get(chrome.extension.getURL('/injected.js'), 
+    function(data) {
+        var script = document.createElement("script");
+        script.setAttribute("type", "text/javascript");
+        script.innerHTML = data;
+        document.getElementsByTagName("head")[0].appendChild(script);
+        document.getElementsByTagName("body")[0].setAttribute("onLoad", "ai_on();");
+    }
+);
